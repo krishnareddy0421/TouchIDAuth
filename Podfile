7f871e2348002d284278ba6031b38fc25f2d5a77
vamsi krishna reddy kamjula <krishnareddy0421@gmail.com>
2017-10-21 10:41:45 -0400

    Pods Added

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.0'
+
+target 'TouchIDAuth' do
+  use_frameworks!
+
+pod 'SwiftKeychainWrapper'
+
+end
---
+++ Pods/Target Support Files/Pods-TouchIDAuth/Pods-TouchIDAuth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TouchIDAuth : NSObject
+@end
+@implementation PodsDummy_Pods_TouchIDAuth
+@end
